09b20019b9c05f2e6c54fd05b4f64909329345e4
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-26 08:17:01 +0700

    add changes

---
+++ src/main/java/com/nyash/rooms/roomsguestservices/repository/GuestRepository.java
@@ -0,0 +1,10 @@
+package com.nyash.rooms.roomsguestservices.repository;
+
+import com.nyash.rooms.roomsguestservices.entity.Guest;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends CrudRepository<Guest, Long> {
+    Guest findByEmailAddress(String emailAddress);
+}
