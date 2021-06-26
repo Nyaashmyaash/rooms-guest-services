b377d90a495ca824161eebdc6cb159a245ca882f
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-26 07:55:26 +0700

    Initial commit

---
+++ src/main/java/com/nyash/rooms/roomsguestservices/RoomsGuestServicesApplication.java
@@ -0,0 +1,13 @@
+package com.nyash.rooms.roomsguestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomsGuestServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomsGuestServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nyash/rooms/roomsguestservices/RoomsGuestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyash.rooms.roomsguestservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomsGuestServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
